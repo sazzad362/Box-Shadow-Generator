e150726a229f25a1d462adaea454b9fd2af091d6
Sazzad Hossain <sazzad362@gmail.com>
2019-11-18 16:35:20 +0600

    Update preview container and color

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,17 +8,32 @@
    *----------------------------------
    */
 	$(document).ready(function() {
-
-	  $('.range-slider__range').change( function(){
-
+  
+	  $('.operational_box').change( function(){
+      
+      // Geting all variables
       var horizontal = $('#horizontal-length').html();
       var vertical   = $('#vertical-length').html();
       var blur       = $('#blur-radius').html();
       var spread     = $('#spread-radius').html();
+      var data       = $('#custom').val();
 
-      $('#box').attr('style', 'box-shadow: '+horizontal+'px '+vertical+'px '+blur+'px '+spread+'px #000');
+      // Store all values
+      var proparty = 'box-shadow: '+horizontal+'px '+vertical+'px '+blur+'px '+spread+'px  '+data+'';
+
+      // Apply on preview area
+      $('#box').attr('style', proparty);
+      // Get data on input field
+      $('#out_code').val(proparty);
 
     })
+
+    //Initialize Spectrum Color Picker
+    $("#custom").spectrum({
+        color: "#000",
+        showAlpha: true,
+        preferredFormat: "rgb",
+    });
 		
 	});// DOM Ready
 
